88647cf879bbfe2c93af88d80b341adf108a9c3a
Sandhani Shaik <sandhani.shaik@trigyn.com>
2022-07-18 16:31:35 +0530

    first commit

---
+++ src/app/cart.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { Product } from "./products";
+import { HttpClient } from '@angular/common/http';
+@Injectable({providedIn: 'root'})
+
+export class CartService {
+  
+  constructor(private http: HttpClient){}
+
+  items: Product[] = [];
+/* . . . */
+
+  addToCart(product: Product) {
+    console.log(product);
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+/* . . . */
+}
